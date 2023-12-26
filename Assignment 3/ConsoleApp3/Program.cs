8b2f8eb3d54dcf6f4f0f93169276be94e44b8887
EmadAbouelsaad <153760847+EmadAbouelsaad@users.noreply.github.com>
2023-12-26 03:30:22 +0100

    Add files via upload

---
+++ Assignment 3/ConsoleApp3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class PrimeSieve
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the upper limit to find prime numbers:");
+
+        if (int.TryParse(Console.ReadLine(), out int limit) && limit >= 2)
+        {
+            bool[] isPrime = new bool[limit + 1];
+
+            // Initialize all numbers as prime
+            for (int i = 2; i <= limit; i++)
+            {
+                isPrime[i] = true;
+            }
+
+            // Apply the Sieve of Eratosthenes
+            for (int i = 2; i <= Math.Sqrt(limit); i++)
+            {
+                if (isPrime[i])
+                {
+                    for (int j = i * i; j <= limit; j += i)
+                    {
+                        isPrime[j] = false;
+                    }
+                }
+            }
+
+            // Print prime numbers
+            Console.WriteLine($"Prime numbers between 1 and {limit} are:");
+
+            for (int i = 2; i <= limit; i++)
+            {
+                if (isPrime[i])
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid positive integer greater than or equal to 2.");
+        }
+    }
+}
